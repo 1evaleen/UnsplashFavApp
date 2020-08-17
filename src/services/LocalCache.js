4e81c8d37ba3699fcc4a40ffe1d82a12ae2b234f
Evaleen <64602269+1evaleen@users.noreply.github.com>
2020-08-17 16:58:38 +0100

    local cache error fix

--- src/services/LocalCache.js
+++ src/services/LocalCache.js
@@ -1,15 +1,17 @@
 const LOCALSTORAGE_KEY = 'photoAppFavourites';
 
 class LocalCache {
-  DEFAULT_DATA = {};
+  static DEFAULT_DATA = {};
 
   static getData = () => {
-    const data = localStorage.getItem(LOCALSTORAGE_KEY);
-    if (!data) {
-      saveData(this.DEFAULT_DATA);
-      return this.DEFAULT_DATA;
+    let data = localStorage.getItem(LOCALSTORAGE_KEY);
+    if(typeof data == 'string') {
+      data = JSON.parse(data);
+    } else {
+      data = LocalCache.DEFAULT_DATA;
     }
-    return JSON.parse(data);
+    LocalCache.saveData(data);
+    return data;
   };
 
   static saveData = data =>
